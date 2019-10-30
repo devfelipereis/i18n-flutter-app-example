8f2fbdd1ec21b5f06c1ccde94d06585d4b773f2a
Felipe Reis <felipereis@localhost.localdomain>
2019-10-30 18:41:46 -0300

    first commit

---
+++ lib/AppLanguage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppLanguage extends ChangeNotifier {
+  Locale _appLocale = Locale('en');
+
+  Locale get appLocal => _appLocale ?? Locale("en");
+
+  fetchLocale() async {
+    var prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('language_code') == null) {
+      _appLocale = Locale('en', 'US');
+      return Null;
+    }
+    _appLocale = Locale(prefs.getString('language_code'));
+    return Null;
+  }
+
+  void changeLanguage(Locale type) async {
+    var prefs = await SharedPreferences.getInstance();
+    if (_appLocale == type) {
+      return;
+    }
+    if (type == Locale("pt", "BR")) {
+      _appLocale = Locale("pt", "BR");
+      await prefs.setString('language_code', 'pt');
+      await prefs.setString('countryCode', 'BR');
+    } else {
+      _appLocale = Locale("en", "US");
+      await prefs.setString('language_code', 'en');
+      await prefs.setString('countryCode', 'US');
+    }
+    notifyListeners();
+  }
+}
